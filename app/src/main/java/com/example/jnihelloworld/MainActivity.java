a69ebd75c92c3ca2087398c18ccd8219b53ce910
zhengcaifu <344984602@qq.com>
2019-11-11 11:16:15 +0800

    布局调整。

--- app/src/main/java/com/example/jnihelloworld/MainActivity.java
+++ app/src/main/java/com/example/jnihelloworld/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jnihelloworld;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
         User user = new User("郑小才", "10010");
         Toast.makeText(this, "int :" + new Jni().getUserName(user), Toast.LENGTH_LONG).show();
     }
